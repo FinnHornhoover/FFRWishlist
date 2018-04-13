866ab6bf8a29ab36a460e597f4671ba45d944a43
FinnHornhoover <finnhornhoover@gmail.com>
2018-04-13 17:22:27 +0300

    updated version

--- src/finnhh/ffrwishlist/MainApp.java
+++ src/finnhh/ffrwishlist/MainApp.java
@@ -51,7 +51,7 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class MainApp extends Application {
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     private DatabaseManager appDatabaseManager;
     private WebUpdater appDBUpdater;
