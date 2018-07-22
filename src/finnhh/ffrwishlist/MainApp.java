39cca492b9dfb01beb4d5108d5a9610192d3684c
FinnHornhoover <finnhornhoover@gmail.com>
2018-07-22 22:47:02 +0300

    updated version

--- src/finnhh/ffrwishlist/MainApp.java
+++ src/finnhh/ffrwishlist/MainApp.java
@@ -52,7 +52,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainApp extends Application {
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     private DatabaseManager appDatabaseManager;
     private WebUpdater appUpdater;
